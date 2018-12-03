a41916eb9ad792e7f281be5000ebe20001d91787
Pelin_li <penglong95.li@gmail.com>
2018-12-03 10:03:15 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # gst
+
+For-Loop create and destroy 
+
---
+++ gst.go
@@ -0,0 +1,43 @@
+package gst
+
+// For 循环协程的创建与销毁
+import (
+	"sync"
+)
+
+var (
+	iMap map[string]chan int
+)
+
+func init() {
+	iMap = make(map[string]chan int)
+}
+
+func NewLoopRoutine(key string, wg *sync.WaitGroup, f func()) {
+	quit := make(chan int)
+	iMap[key] = quit
+
+	go handle(quit, wg, f)
+}
+
+func handle(quit chan int, wg *sync.WaitGroup, f func()) {
+	for {
+		select {
+		case <-quit: {
+			wg.Done()
+			break
+		}
+		default:
+			f()
+		}
+	}
+}
+
+func StopLoopRoutine(key string) {
+	quit := iMap[key]
+	if quit == nil {
+		return
+	}
+	quit <- 1
+}
+
